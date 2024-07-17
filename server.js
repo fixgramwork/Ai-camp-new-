b6837c7624838f2dac3463d892dfd071b9b47a83
fixgramwork <kimminje661@outlook.kr>
2024-07-17 12:30:26 +0900

    2st commit

--- server.js
+++ server.js
@@ -27,43 +27,49 @@ io.on('connection', (socket) => {
     });
 });
 
-app.use(express.static("/Users/kimminje/AI\ CAMP(new)"));
+app.use(express.static("/Users/kimminje/AI\ CAMP(NEW)"));
 
 //home
 app.get("/", (req, res) => {
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/main.html");
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/main.css");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/main.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/main.css");
 });
 
 //로그인
 app.get("/login", (req, res) => {
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/login.html");
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/login.css");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/login.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/login.css");
 });
 
 //챌린지
 app.get("/challenge", (req, res) => {
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/challenge.html");
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/challenge.css");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/challenge.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/challenge.css");
 });
 
 //랭크
 app.get("/rank", (req, res) => {
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/rank.html");
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/rank.css");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/rank.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/rank.css");
 });
 
 //대전
 app.get("/pvp", (req, res) => {
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/pvp.html");
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/pvp.css");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/pvp.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/pvp.css");
 });
 
 //목록, 공유
 app.get("/list", (req, res) => {
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/list.html");
-    res.sendFile("/Users/kimminje/AI-CAMP(new)/list.css");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/list.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/list.css");
 });
+
+//상점
+app.get("/store", (req, res) => {
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW))/store.html");
+    res.sendFile("/Users/kimminje/AI-CAMP(NEW)/store.css");
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
